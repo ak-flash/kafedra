feffe96e0021c99184583a4db2857b05ae384654
ak-flash <ak-flash@mail.ru>
2022-10-07 09:55:37 +0300

    remove deaprtment name from login screen

--- resources/views/vendor/filament/components/brand.blade.php
+++ resources/views/vendor/filament/components/brand.blade.php
@@ -1,8 +1,11 @@
 <div class="flex items-center space-x-4">
     <img src="{{ asset('/images/logo-m.png') }}" alt="Кафедра ВолгГМУ" class="h-10">
 
-    <span class="text-xs">
-        {{ auth()->user()->main_department?->name ?? 'Не указана кафедра' }}
-    </span>
+    @auth
+        <span class="text-xs">
+            {{ auth()->user()->main_department?->name ?? 'Не указана кафедра' }}
+        </span>
+    @endauth
+
 </div>
 
